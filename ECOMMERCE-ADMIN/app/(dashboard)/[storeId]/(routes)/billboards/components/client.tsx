4d1e8e1cd51c83ef716171c6c3de56f23f854816
visvoker <s2089533@gmail.com>
2024-06-19 22:44:23 +0800

    Data Table -1

--- ECOMMERCE-ADMIN/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ECOMMERCE-ADMIN/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,13 +1,22 @@
 "use client"
 
 import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
 
 import { Heading } from "@/components/ui/heading"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
-import { useParams, useRouter } from "next/navigation"
+import { DataTable } from "@/components/ui/data-table"
 
-export const BillboardClient = () => {
+import { BillboardColumn, columns } from "./columns"
+
+interface BillboardClientProps {
+  data: BillboardColumn[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+  data
+}) => {
   const router = useRouter();
   const params = useParams();
 
@@ -15,7 +24,7 @@ export const BillboardClient = () => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboard (0)"
+          title={`Billboards (${data.length})`}
           description="Manage billboard for your store"
         />
         <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
@@ -24,6 +33,7 @@ export const BillboardClient = () => {
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} />
     </>
   )
 }
